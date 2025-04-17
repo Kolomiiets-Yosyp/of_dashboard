32a97b02da82c352e00ffdef56042570c2023bdb
yosyp <kolomiets204@gmail.com>
2025-04-17 22:41:19 +0200

    setting and url update

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,7 +5,7 @@ from .views import general_dashboard, create_user, run_script
 app_name = 'dashboard'
 
 urlpatterns = [
-    path('', general_dashboard, name='general_dashboard'),  # ✅ Оновлена назва
+    path('', general_dashboard, name='general_dashboard'),
     path('user/<int:user_id>/dashboard/', views.user_dashboard, name='user_dashboard'),
     path('dashboard/create_user/', create_user, name='create_user'),
     path('run_script/', run_script, name='run_script'),
